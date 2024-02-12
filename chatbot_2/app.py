17e1d7ec2016c19e9a3d8beed3b486215ffbfb25
Riyad Mehdiyev <104145398+RiyadMehdi7@users.noreply.github.com>
2024-02-12 11:48:31 +0000

    app.py changed

--- chatbot_2/app.py
+++ chatbot_2/app.py
@@ -9,64 +9,39 @@ model = joblib.load("chatbot_2/svm_classifier.pkl")
 vectorizer = joblib.load("chatbot_2/tfidf_vectorizer.pkl")
 
 RESPONSES = {
-    'Promotion': [
-        "Promotion performans və staja əsaslanır. İnkişaf imkanları fərdi performans və şirkətin məqsədləri ilə uyğunluğa əsaslanır.",
-        "Promotion performans və bacarıqlara əsaslanaraq qiymətləndirilir. İnkişaf imkanları performansınıza və bacarıqlərinizə görə mövcud ola bilər.",
-        "Rəhbərlik, problem həll etmə və əməkdaşlarla əməkdaşlıq kimi bacarıqlar promosiyalar üçün dəyərlənir. Promotion prosesi performans qiymətləndirmələri, müsahibələr və rəhbərlik tərəfindən təsdiqlər daxil ola bilər.",
-        "İnkişaf imkanları performansınıza və bacarıqlərinizə görə dəyişə bilər.",
-        "Promotion vaxtı fərdi performans və təşkilati amillərə görə dəyişə bilər.",
-        "Problem həll etmə və əməkdaşlıq kimi bacarıqlar promotion üçün dəyərlənir.",
-        "Promotion üçün namizədlər performanslarına, potensiallarına və təşkilati məqsədlərlə uyğunluğuna görə müəyyən edilə bilər.",
-        "Promotion prosesi performans qiymətləndirmələri, müsahibələr və rəhbərlik tərəfindən təsdiqlər daxil ola bilər.",
-        "Promotion üçün hazır olmağı nümayiş etdirmək üçün daima gözləntiləri aşmaq, əlavə məsuliyyətləri götürmək və büyümə imkanları axtarmaq lazımdır.",
-        "Promotion məqsədləri üçün performans, rəhbərlik bacarıqləri və şirkətin dəyərləri kimi mənbələrə əsaslanan performans ölçülməsi aparıla bilər.",
-        "Promotion-un faydaları arasında məsuliyyət artımı, yüksək əmək haqqı və karyera imkanları olabilir.",
-        "Promotion-un vaxtı fərdi performans, təşkilati ehtiyaclar və mövcud imkanlar əsasında dəyişə bilər.",
-        "Yüksək performans göstərənlər promotion, bonuslarla mükafatlandırıla bilərlər.",
-        "Karyera imkanları promotion və xüsusi təlim proqramlarından ibarət ola bilər.",
-        "Əməkdaşlar karyera inkişafı üçün təlim proqramları, mentorluq, fərdi inkşaf modeli kimi dəstək proqramları ilə fayndalana bilərlər.",
+    'Əsas fəaliyyət göstəriciləri (KPİ)': [
+        "KPİ-lər, əməkdaşların hər birinin vəzifələrinə uyğun olaraq təyin olunan məqsədlərə necə nail olduğunu qiymətləndirməyə kömək edir.",
+        "ƏFG-lər, şirkətin əsas məqsədlərinə nail olmaq üçün hər bir əməkdaşın performansını izləmək üçün istifadə olunan rəsmi göstəricilərdir.",
+        "KPİ-lər, əməkdaşların hər birinin iş performansını, məqsədlərə nail olmaqda necə yardım etdiklərini və işlərinin effektivliyini qiymətləndirmək üçün istifadə olunan müvafiq parametrlərdir.",
+        "ƏFG-lər, əməkdaşların iş performansını, şirkətin əsas hədəflərinə uyğunluğunu və effektivliyini ölçmək üçün təyin olunan əsas göstəricilərdir.",
+        "KPİ-lər, əməkdaşların iş performansını izləmək və şirkətin nailiyyətini ölçmək üçün istifadə olunan müəyyən məlumat və rəqəmlərdən ibarətdir.",
+        "ƏFG-lər, əməkdaşların işdəki məhsuldarlığını və verimliliyini ölçmək üçün müəyyən edilmiş dəyərlərdən ibarətdir.",
+        "KPİ-lər, əməkdaşların iş təcrübəsini, performansını və əməkdaşlığını qiymətləndirmək üçün təyin olunan standart göstəricilərdən ibarətdir.",
+        "ƏFG-lər, əməkdaşların şirkətin əsas məqsədlərinə necə nail olduğunu izləmək üçün müəyyən edilmiş göstəricilərdir.",
+        "KPİ-lər, əməkdaşların işdəki məhsuldarlığını ölçmək, işlərə necə yanaşdığını izləmək və şirkətin ümumi nailiyyətini qiymətləndirmək üçün istifadə olunan əsas məlumatlardır."
     ],
-    'Salary': [
-        "Əmək haqqı iş rolü, təcrübə və bazar qiymətlərinə görə müəyyənləşdirilir. Bonuslar və ya təşviqatlar fərdi və şirkət performansına görə mövcud ola bilər.",
-        "Əmək haqqı dəyişiklikləri illik olaraq və ya performans əsasında baş verə bilər. Əmək haqqı müzakirəsi bazar qiymətləri və sizin kifayət qədərliyiniz kimi müxtəlif amillərə görə mümkün ola bilər.",
-        "Orta əmək haqqı yerləşmə, sənaye və təcrübə səviyyəsi kimi amillərə görə dəyişə bilər. İstifadə müqavilənizə bağlı olaraq, sığorta, pensiya planları və digər şəxsiyyətə görə əvəzlər kimi faydalar ola bilər.",
-        "Əmək haqqı müzakirəsi bazar qiymətləri və sizin kifayət qədərliyiniz kimi müxtəlif amillərə görə mümkün ola bilər.",
-        "İstifadə müqavilənizə bağlı olaraq, sığorta, pensiya planları və digər şəxsiyyətə görə əvəzlər kimi faydalar ola bilər.",
-        "Orta əmək haqqı yerləşmə, sənaye və təcrübə səviyyəsi kimi amillərə görə dəyişə bilər.",
-        "Əmək haqqınızla bağlı məsələləri rəhbərinizlə müzakirə edə və əgər lazımsızsa əmək haqqı rəyini müzakirə edə bilərsiniz.",
-        "Əmək haqqı aralığı yerləşmə, sənaye və şirkətin ölçüsü kimi amillərə görə dəyişə bilər.",
-        "Əmək haqqı həyat keyfiyyətinin artımı üçün müəyyən mənzillərdə fərqli olaraq müdrikləşmələr etmək üçün müvafiq olaraq hesablanıra.",
-        "Siz ətrafında sığorta və ya pensiya şəxsiyyətinin müqaviləsindən olan bəzi hissələri, müzakirə edə və rəhbərinizlə razılaşmaq mümkündür.",
-        "Əmək haqqı artımları performans, bazar trendləri və şirkətin mənfəətə görə müəyyən edilə bilər.",
-        "Əgər performans baxışınızdan razı deyilsinizsə, narahatlıqlarınızı rəhbərinizlə müzakirə edə və həll etmək üçün əlavə məlumatlar təmin edə bilərsiniz.",
-        "İllik əmək haqqı artımları şirkət performansı, bazar trendləri və fərdi performans kimi amillərə görə təyin oluna bilər.",
-        "Bu rolda əmək haqqı artımı fərdi performans, təcrübə və şirkət siyasətlərinə görə dəyişə bilər.",
-        "Əmək haqqından başqa, faydalar sağlık sığortası, pensiya planları, ödənilmiş iş vaxtı və digər əvəzlər kimi dəstəklər daxil ola bilər.",
-        "Performans baxışlarına aid mübahisələr HR və ya şirkət siyasətinə bağlı olaraq daxilən HR və ya daxili şikayət prosesləri ilə həll edilə bilər.",
-        "Uzaq işçilər üçün performans baxışları, köhnədici görüşlər, performans izləmə proqramları və menecerlərlə düzgün əlaqələrin təmin edilməsi ilə həyata keçirilə bilər.",
-        "Əmək haqqı şəffaflığına dair şirkət siyasəti dəyişiklik göstərə bilər, lakin bəzi şirkətlər əmək haqqı aralığı və mükafat tərzləri haqqında şəffaflıq tətbiq edir.",
-        "Əgər gözləntiləri aşmaq və ya vacib kilometr daşları əldə etmək kimi istənilən hallarda rəhbərinizlə performansə əsaslanan bonus haqqında müzakirə edə bilərsiniz.",
-        "Bu bölmədəki əməkdaşlar üçün əmək haqqı artımı fərdi performans, iş səviyyəsi və şirkətin performansı kimi amillərə görə dəyişə bilər.",
+    'Kompetensiya': [
+        "Kompetensiyalar hər hansı bir vəzifənin icrası üçün tələb olunan səriştələr toplusudur. Əməkdaşların hər hansı bir vəzifədə fəaliyyət göstərməsi və müvəffəqiyyətli olması üçün ehtiyac duyulan səriştələrdir.",
+        "Kompetensiyalar, hər hansı bir vəzifənin icrası üçün tələb olunan səriştələr toplusudur və əməkdaşların müvəffəqiyyətli olması üçün vacibdir.",
+        "Kompetensiyalar, əməkdaşların müvəffəqiyyətli iş icrası üçün tələb olunan səriştələr və bacarıqlər toplusudur.",
+        "Kompetensiyalar, hər hansı bir vəzifənin icrası üçün tələb olunan səriştələr və bacarıqlər toplusudur.",
+        "Kompetensiyalar, əməkdaşların müvəffəqiyyətli iş icrası üçün tələb olunan səriştələr və bacarıqlər toplusudur.",
+        "Kompetensiyalar, əməkdaşların hər hansı bir vəzifədə müvəffəqiyyətli iş icrası üçün tələb olunan səriştələr və bacarıqlər toplusudur."
     ],
-    'Performance Review': [
-        "Performans məqsədləri adətən rəhbərinizlə birgə müəyyən edilir və təşkilati məqsədlərlə uyğunlaşdırılır. Performans baxışında rəhbəriniz performansınızı qiymətləndirəcək, feedback verəcək və gələcəyə dair məqsədləri müzakirə edəcək.",
-        "Mənfi feedback-u inkişaf və öyrənmə imkanı olaraq istifadə edin. Rəhbərinizlə iyşə iş yerində inkişaf etmək üçün sahələri müzakirə edin və onları həll etmək üçün bir plan yaradın. Əgər pis performans baxışı alırsınızsa, bunu inkişaf və yaxşılaşma imkanı kimi istifadə edin.",
-        "Siz müzakirələrdə hansısa məsələləri müzakirə edə və nəzərə almaq üçün əlavə məzmun təqdim edə bilərsiniz.",
-        "Performans baxışları adətən illik olaraq planlaşdırılır.",
-        "Performans baxışında rəhbəriniz performansınızı qiymətləndirəcək, feedback verəcək və gələcəyə dair məqsədləri müzakirə edəcək.",
-        "İnkişaf imkanları performansınıza və bacarıqlərinizə görə mövcud ola bilər.",
-        "Feedback-u inkişaf və öyrənmə imkanı kimi istifadə edin.",
-        "Performans məqsədləri adətən rəhbərinizlə birgə müəyyən edilir və təşkilati məqsədlərlə uyğunlaşdırılır.",
-        "Əgər performans baxışınızı qaçırsınızsa, ən tez zamanda qayıdıb feedback alaraq və gələcəyə dair məqsədləri müzakirə edərək, feedback əldə etmək və məqsədlər qoymaq üçün əsas yaradın.",
-        "Yüksək performans promouşn, bonuslar, mükafatlar və ya başqa tanınma forması vasitəsilə tanınaraq mükafatlandırıla bilər.",
-        "Performans baxışında rəhbəriniz performansınızı qiymətləndirəcək, feedback verəcək və gələcəyə dair məqsədləri müzakirə edəcək.",
-        "Əgər performans reytinqinizlə razılaşmırsınızsa, narahatlıqlarınızı rəhbərinizlə müzakirə edərək və öz məqamlarınızı dəstəkləmək üçün əlavə sübut təqdim edərək, məsələni müzakirə edə bilərsiniz.",
-        "Promotion məqsədləri üçün performans, keyfiyyət və komanda işi kimi mənbələrə əsaslanan performans ölçülməsi aparıla bilər.",
-        "Əgər mənfi feedback alırsınızsa, bunu inkişaf və yaxşılaşma imkanı kimi istifadə edin. Rəhbərinizləinkişaf sahələrini müzakirə edin və həll planı yaradın.",
-        "Komanda üçün performans məqsədləri adətən departament və ya təşkilat məqsədləri ilə uyğunlaşdırılır və birgə müəyyən edilir.",
-        "Əgər performans baxışınızdan razı deyilsinizsə, narahatlıqlarınızı rəhbərinizlə müzakirə edərək və həll etmək üçün əlavə məlumatlar təmin edə bilərsiniz.",
+    'Yekun fəaliyyət balı': [
+        "Yekun fəaliyyət balı, əməkdaşın ƏFG və kompetensiyalar üzrə yekun qiymətləndirmə nəticəsində formalaşan göstəricidir.",
+        "Yekun fəaliyyət balı, əməkdaşın ƏFG və kompetensiyalar üzrə yekun qiymətləndirməsinin nəticəsində formalaşan bir göstəricidir."
     ],
+    'Birbaşa rəhbər': [
+        "Birbaşa rəhbər, əməkdaşın bilavasitə tabe olduğu ilkin rəhbərdir.",
+        "Birbaşa rəhbər, əməkdaşın doğrudan tabe olduğu ilkin rəhbərdir.",
+        "Birbaşa rəhbər, əməkdaşın ən yaxın rəhbəri, onun doğrudan tabe olduğu ilkin rəhbərdir.",
+        "Birbaşa rəhbər, əməkdaşın doğrudan tabe olduğu ilkin rəhbərdir və onunla ən yaxın əlaqə saxlayan şəxsdir.",
+        "Birbaşa rəhbər, əməkdaşın doğrudan tabe olduğu ilkin rəhbərdir və iş əlaqələrində ən yaxın şəxstir.",
+        "Birbaşa rəhbər, əməkdaşın doğrudan tabe olduğu ilkin rəhbərdir və işlə bağlı ən yaxın dəstək verən şəxsdir."
+    ]
 }
+
 from flask import Flask, request, render_template, redirect, url_for, session
 
 app = Flask(__name__)
@@ -110,7 +85,7 @@ def home():
 
 from fuzzywuzzy import process
 
-keywords = ['promotion', 'salary', 'performance', 'wage']
+keywords = ['rəhbər', 'birbaşa rəhbər', 'kpi', 'əfg', 'kompetensiya']
 
 def get_closest_match(input_word):
     closest_match = process.extractOne(input_word, keywords)
